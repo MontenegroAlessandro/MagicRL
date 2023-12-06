247a5bcabc71bc81e83cabce3740bb5c676e2b30
MontenegroAlessandro <alessandro.montenegro@polimi.it>
2023-12-06 16:29:13 +0100

    REINFORCE structure

--- algorithms/policy_gradient.py
+++ algorithms/policy_gradient.py
@@ -22,7 +22,9 @@ class PolicyGradient:
             self, lr: float = 1e-3,
             lr_strategy: str = "constant",
             estimator_type: str = "REINFORCE",
+            initial_theta: np.array = None,
             ite: int = 100,
+            batch_size: int = 1,
             env: BaseEnv = None,
             policy: BasePolicy = None,
             data_processor: BaseProcessor = IdentityDataProcessor(),
@@ -44,6 +46,11 @@ class PolicyGradient:
         assert estimator_type in ["REINFORCE", "GPOMDP"], err_msg
         self.estimator_type = estimator_type
 
+        err_msg = "[PG] initial_theta has not been specified!"
+        assert initial_theta is not None, err_msg
+        self.thetas = np.array(initial_theta)
+        self.dim = len(self.thetas)
+
         assert env is not None
         self.env = env
 
@@ -58,10 +65,15 @@ class PolicyGradient:
 
         # Other class' parameters
         self.ite = ite
+        self.batch_size = batch_size
         self.verbose = verbose
         self.natural = natural
         self.checkpoint_freq = checkpoint_freq
 
         # Useful structures
-
+        self.theta_history = np.zeros((self.ite, self.dim), dtype=float)
+        self.time = 0
+        self.performance_idx = np.zeros(ite, dtype=float)
+        self.best_theta = np.zeros(self.dim, dtype=float)
+        self.best_performance_theta = -np.inf
         return
